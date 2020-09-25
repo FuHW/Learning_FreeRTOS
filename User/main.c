39cb9c820638e53ad87181e788b6ba3714091491
fhw <fhw@juntengtel.com>
2020-09-25 17:16:35 +0800

    project base

---
+++ User/main.c
@@ -0,0 +1,21 @@
+/*
+ * @Author: yoyo
+ * @Date: 2020-09-25 16:55:15
+ * @LastEditTime: 2020-09-25 17:10:49
+ * @LastEditors: yoyo
+ * @Description: 
+ * @FilePath: \SimulateFreeRTOS\User\main.c
+ */
+
+/**
+ * @description: 
+ * @param {type} 
+ * @return {type} 
+ */
+int main(void)
+{
+    for(;;)
+    {
+        /* do nonthing */
+    }
+}
